dfcc0a518b6c06746d39378b2e83546ce5bc8ef8
bertRC <sithalbert@ya.ru>
2019-12-07 17:46:35 +0300

    FrontController added

---
+++ web-app/src/main/java/ru/itpark/web/servlet/FrontController.java
@@ -0,0 +1,25 @@
+package ru.itpark.web.servlet;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import ru.itpark.web.router.Router;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Singleton
+public class FrontController extends HttpServlet {
+    private Router router;
+
+    @Inject
+    public void setRouter(Router router) {
+        this.router = router;
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        log("service");
+        router.route(req, resp);
+    }
+}
---
+++ web-app/src/main/java/ru/itpark/web/servlet/FrontControllerConfig.java
@@ -0,0 +1,24 @@
+package ru.itpark.web.servlet;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+import ru.itpark.MyService;
+import ru.itpark.MyServiceImpl;
+import ru.itpark.web.router.Router;
+import ru.itpark.web.router.RouterHelloWorldImpl;
+
+public class FrontControllerConfig extends GuiceServletContextListener {
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(new ServletModule() {
+            @Override
+            protected void configureServlets() {
+                serve("/").with(FrontController.class);
+                bind(Router.class).to(RouterHelloWorldImpl.class);
+                bind(MyService.class).to(MyServiceImpl.class);
+            }
+        });
+    }
+}
