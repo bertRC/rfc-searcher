da746299e48acc9025991a9833a1d7a4dffcd0b6
bertRC <sithalbert@ya.ru>
2019-12-07 18:17:15 +0300

    some modification

--- rfc-service/src/main/java/ru/itpark/MyServiceImpl.java
+++ rfc-service/src/main/java/ru/itpark/MyServiceImpl.java
@@ -3,6 +3,6 @@ package ru.itpark;
 public class MyServiceImpl implements MyService {
     @Override
     public String printMessage() {
-        return "Hello World!!!";
+        return "Привет Мир!!!";
     }
 }
