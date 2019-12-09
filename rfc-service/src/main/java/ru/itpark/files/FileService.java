bb7a9df964137c73694e26e3bdbdece0763e6514
bertRC <sithalbert@ya.ru>
2019-12-09 16:47:20 +0300

    some methods added

--- rfc-service/src/main/java/ru/itpark/files/FileService.java
+++ rfc-service/src/main/java/ru/itpark/files/FileService.java
@@ -7,4 +7,12 @@ public interface FileService {
     String writeFile(Part part);
 
     void writeFiles(List<Part> parts);
+
+    List<String> getAll();
+
+    boolean removeFile(String name);
+
+    void removeAll();
+
+    void downloadRfcFromUrl(String urlRegex, int fromIndex, int toIndex);
 }
