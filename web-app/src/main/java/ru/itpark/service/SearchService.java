26f0ce6b266a93a4b749a462e3c5af038eb17f48
bertRC <sithalbert@ya.ru>
2019-12-15 22:09:06 +0300

    some methods added

--- web-app/src/main/java/ru/itpark/service/SearchService.java
+++ web-app/src/main/java/ru/itpark/service/SearchService.java
@@ -3,9 +3,14 @@ package ru.itpark.service;
 import ru.itpark.model.QueryModel;
 
 import java.util.List;
+import java.util.Map;
 
 public interface SearchService {
     List<QueryModel> getAllQueries();
 
-    void doSearch(String text);
+    Map<String, String> getAllTaskPercents();
+
+    String getTaskPercent(String id);
+
+    void search(String text);
 }
--- web-app/src/main/java/ru/itpark/service/SearchServiceDefaultImpl.java
+++ web-app/src/main/java/ru/itpark/service/SearchServiceDefaultImpl.java
@@ -1,22 +1,29 @@
 package ru.itpark.service;
 
 import com.google.inject.Inject;
+import lombok.val;
 import ru.itpark.enumeration.QueryStatus;
 import ru.itpark.file.FileService;
 import ru.itpark.model.QueryModel;
 import ru.itpark.repository.QueryRepository;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.stream.Collectors;
 
 public class SearchServiceDefaultImpl implements SearchService {
     private FileService fileService;
     private QueryRepository queryRepository;
 
     private Thread searchThread;
-    //    private final List<QueryModel> currentQueries = new LinkedList<>();
     private final BlockingDeque<QueryModel> currentQueries = new LinkedBlockingDeque<>();
+    private final ConcurrentMap<String, Integer> taskPercent = new ConcurrentHashMap<>();
 
     @Inject
     public void setFileService(FileService fService) {
@@ -38,13 +45,67 @@ public class SearchServiceDefaultImpl implements SearchService {
     }
 
     @Override
-    public void doSearch(String text) {
+    public Map<String, String> getAllTaskPercents() {
+        HashMap<String, String> result = new HashMap<>();
+        taskPercent.forEach((k, v) -> result.put(k, v.toString()));
+        return result;
+    }
+
+    @Override
+    public String getTaskPercent(String id) {
+        return taskPercent.get(id).toString();
+    }
+
+    @Override
+    public void search(String text) {
         currentQueries.addFirst(new QueryModel(
                 UUID.randomUUID().toString(),
                 text,
                 QueryStatus.ENQUEUED));
-        if (searchThread == null || !searchThread.isAlive()) {
-            //можно делать работу, а иначе запрос просто повисит в currentQueries
+        new Thread(this::doSearch).start();
+    }
+
+    private void doSearch() {
+        if (searchThread != null && searchThread.isAlive()) {
+            //need to wait until done
+//                searchThread.join();
+            return;
+        }
+        if (currentQueries.isEmpty()) {
+            return;
+        }
+        try {
+            QueryModel lastQuery = currentQueries.takeLast();
+            if (lastQuery.getStatus() != QueryStatus.ENQUEUED) {
+                throw new RuntimeException("Status must be ENQUEUED!");
+            }
+            lastQuery.setStatus(QueryStatus.INPROGRESS);
+            currentQueries.addLast(lastQuery);
+            taskPercent.put(lastQuery.getId(), 0);
+            searchThread = new Thread(() -> {
+                //searching in all files
+                String id = lastQuery.getId();
+                String text = lastQuery.getQuery();
+                        Path rfcPath = fileService.getRfcPath();
+                try (val paths = Files.list(rfcPath)) {
+                    long count = paths.count();
+                    //ищем text во всех файлах, формируем результат, сохраняем
+                    //обновляем проценты
+                    //проверяем на interrupt
+                    //под конец удаляем процент
+                    //делаем статус DONE
+                    //отправляем в БД
+                    //удаляем из currentQueries
+                    //searchThread = null;
+                    //запускаем doSearch
+                    //new Thread(this::doSearch).start();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            searchThread.start();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
