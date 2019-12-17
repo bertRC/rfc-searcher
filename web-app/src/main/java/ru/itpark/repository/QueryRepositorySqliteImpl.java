5d6e603f9353708d8a84132a5e0caca7b6667308
bertRC <sithalbert@ya.ru>
2019-12-17 19:40:29 +0300

    some modification

--- web-app/src/main/java/ru/itpark/repository/QueryRepositorySqliteImpl.java
+++ web-app/src/main/java/ru/itpark/repository/QueryRepositorySqliteImpl.java
@@ -9,7 +9,6 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.List;
-//import java.util.UUID;
 
 public class QueryRepositorySqliteImpl implements QueryRepository {
     private JdbcTemplate template = new JdbcTemplate();
@@ -57,24 +56,6 @@ public class QueryRepositorySqliteImpl implements QueryRepository {
                 stmt.setString(3, queryModel.getStatus().toString());
                 return stmt;
             });
-
-//            if (queryModel.getId() == null) {
-//                String id = UUID.randomUUID().toString();
-//                template.update(ds, "INSERT INTO queries(id, query, status) VALUES (?, ?, ?);", stmt -> {
-//                    stmt.setString(1, id);
-//                    stmt.setString(2, queryModel.getQuery());
-//                    stmt.setString(3, queryModel.getStatus().toString());
-//                    return stmt;
-//                });
-//                queryModel.setId(id);
-//            } else {
-//                template.update(ds, "UPDATE queries SET query = ?, status = ? WHERE id = ?;", stmt -> {
-//                    stmt.setString(1, queryModel.getQuery());
-//                    stmt.setString(2, queryModel.getStatus().toString());
-//                    stmt.setString(3, queryModel.getId());
-//                    return stmt;
-//                });
-//            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
